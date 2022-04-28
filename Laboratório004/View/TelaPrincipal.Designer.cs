f5f67cb571467eaec7697444beab1f228fa08de2
vinicius.areboucas <vinicius.areboucas@SBC0505229W10-1>
2022-04-28 15:59:39 -0300

    Adicionar arquivos de projeto.

---
+++ Laboratório004/View/TelaPrincipal.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Laboratório004.View
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.tbxNumero = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(56, 44);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(106, 15);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Digite um Numero";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lblResultado.Location = new System.Drawing.Point(44, 118);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(270, 87);
+            this.lblResultado.TabIndex = 1;
+            // 
+            // tbxNumero
+            // 
+            this.tbxNumero.Location = new System.Drawing.Point(56, 72);
+            this.tbxNumero.Name = "tbxNumero";
+            this.tbxNumero.Size = new System.Drawing.Size(169, 23);
+            this.tbxNumero.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(259, 72);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 288);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbxNumero);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblTitulo);
+            this.KeyPreview = true;
+            this.Name = "TelaPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema De Calculo Quadrado";
+            this.Load += new System.EventHandler(this.TelaPrincipal_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TelaPrincipal_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox tbxNumero;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ Laboratório004/View/TelaPrincipal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratório004.View
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void TelaPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if(tbxNumero.Text == "")
+            {
+                MessageBox.Show("Insira um número");
+
+            }
+            else
+            { 
+            int numero = 0;
+            int quadrado = 0;
+
+            numero = Convert.ToInt32(tbxNumero.Text);
+
+            quadrado = numero * numero;
+
+            lblResultado.Text = "o número" + numero + "ao quadrado é" + quadrado.ToString();
+                tbxNumero.Focus();
+                tbxNumero.SelectAll();
+                
+            }
+        }
+
+        private void TelaPrincipal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+                {
+                e.Handled = true;
+
+                if (e.KeyChar == 13)
+                { 
+                    btnCalcular.PerformClick();
+                }
+            }
+        }
+    }
+}
